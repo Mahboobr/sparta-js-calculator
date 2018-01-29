5ff4f44691ed6628a6e0d82d7f58c127b85a9782
Tech-A28 <tech-a28@Tech-A28s-MacBook-Air.local>
2018-01-29 21:43:39 +0000

    calculator basic and advanced

---
+++ app.js
@@ -0,0 +1,44 @@
+var CalcType = prompt("Which type of calculator would you like to use? Basic(b) or Advanced(a)");
+
+var total = 0;
+if (CalcType == "b") {
+  var InputNum = prompt("Input 1st number");
+  var num1 = parseInt(InputNum, 10);
+
+  var operator = prompt("Select operator. + - / *");
+
+  var InputNum2 = prompt("Input 2nd number");
+  var num2 = parseInt(InputNum2, 10);
+
+  if (operator == "+" && (num2)) {
+    total = num1 + num2;
+    alert(total);
+  } else if (operator == "-" && (num2)) {
+    total = num1 - num2;
+    alert(total);
+  }else if (operator == "*" && (num2)) {
+    total = num1 * num2;
+    alert(total);
+  }else if (operator == "/" && (num2)) {
+    total = num1 / num2;
+    alert(total);
+  }
+} else if (CalcType == "a") {
+  var option = prompt("Power or SquareRoot");
+  switch (option) {
+    case "Power":
+      var InputNum = prompt("Input 1st number");
+      var num1 = parseInt(InputNum, 10);
+      var InputNum2 = prompt("Input 2nd number");
+      var num2 = parseInt(InputNum2, 10);
+      total = Math.pow(num1, num2);
+      alert(total);
+      break;
+    case "SquareRoot":
+      var InputNum = prompt("Input 1st number");
+      var num1 = parseInt(InputNum, 10);
+      total = Math.sqrt(num1);
+      alert(total);
+      break;
+  }
+}
